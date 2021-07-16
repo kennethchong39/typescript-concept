bd0b836501d1a07701b9f0e306b519368503e9bd
Kenneth Chong <kchong@rivian.com>
2021-07-15 17:28:07 -0700

    basic types

--- Basic Types/app.js
+++ Basic Types/app.js
@@ -1,4 +1,4 @@
-// string, number, boolean is types in typescript. All lowercase.
+// ******* string, number, boolean is types in typescript. All lowercase.
 function add(n1, n2, showResult) {
     return n1 + n2;
 }
@@ -6,7 +6,7 @@ var number1 = 5;
 var number2 = 2.8;
 var result = add(number1, number2);
 console.log(result);
-// Object Types
+// ******* Object Types
 // { age: 30 }
 var person = {
     name: 'Max',
@@ -22,5 +22,75 @@ var product = {
         description: 'A great carpet - almost brand-new!'
     }
 };
-// Array
+// ******* Array
 var arr = [1, 2, 3, 4];
+// Tuple
+// Special type to tell you that role[0] is number and role[1] is string
+var role = [2, 'author'];
+// ******* Enum
+// after = is your value you want to assign to the enum
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role["READ_ONLY"] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
+var roleEnum = Role.ADMIN;
+// ******* Any
+// Any kind of value
+var test = 123;
+function combine(input1, input2, resultConversion) {
+    var result;
+    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
+        resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+}
+// ***** Function Return Types & "void"
+function addition(n1, n2) {
+    return n1 + n2;
+}
+function printResult(num) {
+    console.log('Result:' + num);
+}
+// undefined = return empty;
+function printResult2(num) {
+    console.log('Result:' + num);
+    return;
+}
+printResult(addition(5, 12));
+// ******* Function as Types
+// This is not precise
+// let combineValues: Function;
+// combineValues = add;
+// console.log(combineValues(8, 8));
+// This is precise
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
+// ******* unknown
+//  unknwon is more restrictive than any type
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+// This won't work so you need to do extra type check like below (if loop);
+// userName = userInput;
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+// ***** never
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occurred!', 500);
--- Basic Types/app.ts
+++ Basic Types/app.ts
@@ -1,4 +1,4 @@
-// string, number, boolean is types in typescript. All lowercase.
+// ******* string, number, boolean is types in typescript. All lowercase.
 function add(n1: number, n2: number, showResult?: boolean) {
   return n1 + n2;
 }
@@ -9,7 +9,7 @@ const number2 = 2.8;
 const result: number = add(number1, number2);
 console.log(result);
 
-// Object Types
+// ******* Object Types
 // { age: 30 }
 const person: { name: string; age: number } = {
   name: 'Max',
@@ -36,14 +36,14 @@ const product: {
   },
 };
 
-// Array
+// ******* Array
 const arr: number[] = [1, 2, 3, 4];
 
 // Tuple
 // Special type to tell you that role[0] is number and role[1] is string
 const role: [number, string] = [2, 'author'];
 
-// Enum
+// ******* Enum
 // after = is your value you want to assign to the enum
 enum Role {
   ADMIN = 5,
@@ -53,11 +53,11 @@ enum Role {
 
 const roleEnum = Role.ADMIN;
 
-// Any
+// ******* Any
 // Any kind of value
 const test: any = 123;
 
-// Union
+// ******* Union
 // function combine(input1: number | string, input2: number | string) {
 //   let result;
 
@@ -76,10 +76,46 @@ const test: any = 123;
 // const combinedNames = combine('Max', 'Anna');
 // console.log(combinedNames);
 
-// Literal
+// ***** Literal
+// function combine(
+//   input1: number | string,
+//   input2: number | string,
+//   resultConversion: string
+// ) {
+//   let result;
+
+//   if (
+//     (typeof input1 === 'number' && typeof input2 === 'number') ||
+//     resultConversion === 'as-number'
+//   ) {
+//     result = +input1 + +input2;
+//   } else {
+//     result = input1.toString() + input2.toString();
+//   }
+
+//   // if (resultConversion === 'as-number') {
+//   //   return +result;
+//   // } else {
+//   //   return result.toString();
+//   // }
+// }
+
+// const combinedAges = combine(30, 25, 'as-number');
+// console.log(combinedAges);
+
+// const combinedStringAges = combine('30', 25, 'as-number');
+// console.log(combinedStringAges);
+
+// const combinedNames = combine('Max', 'Anna', 'as-text');
+// console.log(combinedNames);
+
+// **** Type Aliases
+
+type Combinable = number | string;
+
 function combine(
-  input1: number | string,
-  input2: number | string,
+  input1: Combinable,
+  input2: Combinable,
   resultConversion: string
 ) {
   let result;
@@ -92,19 +128,69 @@ function combine(
   } else {
     result = input1.toString() + input2.toString();
   }
-
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
 }
 
-const combinedAges = combine(30, 25, 'as-number');
-console.log(combinedAges);
+// ***** Function Return Types & "void"
 
-const combinedStringAges = combine('30', 25, 'as-number');
-console.log(combinedStringAges);
+function addition(n1: number, n2: number): number {
+  return n1 + n2;
+}
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+function printResult(num: number): void {
+  console.log('Result:' + num);
+}
+
+// undefined = return empty;
+function printResult2(num: number): undefined {
+  console.log('Result:' + num);
+  return;
+}
+
+printResult(addition(5, 12));
+
+// ******* Function as Types
+// This is not precise
+// let combineValues: Function;
+
+// combineValues = add;
+
+// console.log(combineValues(8, 8));
+
+// This is precise
+let combineValues: (n1: number, n2: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
+
+// ******* unknown
+//  unknwon is more restrictive than any type
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+// This won't work so you need to do extra type check like below (if loop);
+// userName = userInput;
+
+if (typeof userInput === 'string') {
+  userName = userInput;
+}
+
+// ***** never
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+const res = generateError('An error occurred!', 500);
+console.log(res);

513525cf24ccbb4bc7780837b2aa9bf940364ee9
Kenneth Chong <kchong@rivian.com>
2021-07-24 20:02:53 -0700

    refactor code

--- dist/Basic Types/basic-types.js
+++ dist/Basic Types/basic-types.js
@@ -1,32 +1,33 @@
+"use strict";
 // ******* string, number, boolean is types in typescript. All lowercase.
 function add(n1, n2, showResult) {
     return n1 + n2;
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
+const number1 = 5;
+const number2 = 2.8;
+const result = add(number1, number2);
 console.log(result);
 // ******* Object Types
 // { age: 30 }
-var person = {
+const person = {
     name: 'Max',
-    age: 30
+    age: 30,
 };
 console.log(person);
-var product = {
+const product = {
     id: 'abc1',
     price: 12.99,
     tags: ['great-offer', 'hot-and-new'],
     details: {
         title: 'Red Carpet',
-        description: 'A great carpet - almost brand-new!'
-    }
+        description: 'A great carpet - almost brand-new!',
+    },
 };
 // ******* Array
-var arr = [1, 2, 3, 4];
+const arr = [1, 2, 3, 4];
 // Tuple
 // Special type to tell you that role[0] is number and role[1] is string
-var role = [2, 'author'];
+const role = [2, 'author'];
 // ******* Enum
 // after = is your value you want to assign to the enum
 var Role;
@@ -35,12 +36,12 @@ var Role;
     Role["READ_ONLY"] = "READ_ONLY";
     Role["AUTHOR"] = "AUTHOR";
 })(Role || (Role = {}));
-var roleEnum = Role.ADMIN;
+const roleEnum = Role.ADMIN;
 // ******* Any
 // Any kind of value
-var test = 123;
+const test = 123;
 function combine(input1, input2, resultConversion) {
-    var result;
+    let result;
     if ((typeof input1 === 'number' && typeof input2 === 'number') ||
         resultConversion === 'as-number') {
         result = +input1 + +input2;
@@ -68,20 +69,20 @@ printResult(addition(5, 12));
 // combineValues = add;
 // console.log(combineValues(8, 8));
 // This is precise
-var combineValues;
+let combineValues;
 combineValues = add;
 console.log(combineValues(8, 8));
 function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
+    const result = n1 + n2;
     cb(result);
 }
-addAndHandle(10, 20, function (result) {
+addAndHandle(10, 20, (result) => {
     console.log(result);
 });
 // ******* unknown
 //  unknwon is more restrictive than any type
-var userInput;
-var userName;
+let userInput;
+let userName;
 userInput = 5;
 userInput = 'Max';
 // This won't work so you need to do extra type check like below (if loop);
@@ -93,4 +94,5 @@ if (typeof userInput === 'string') {
 function generateError(message, code) {
     throw { message: message, errorCode: code };
 }
-generateError('An error occurred!', 500);
+const res = generateError('An error occurred!', 500);
+console.log(res);
